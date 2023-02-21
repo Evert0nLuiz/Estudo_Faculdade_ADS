9b3594088a6951bfaa6841459875e369c16f5272
Everton <76123738+Vzuviiu@users.noreply.github.com>
2023-02-20 21:18:22 -0300

    sum test in java

--- Estudo.java
+++ Estudo.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
 public class Test
-
 {
     public static void main(String[] args)
     {
         int n1;
         int n2;
         int res;
-
         Scanner scn = new Scanner(System.in);
 
-        System.out.println("Hi!");
 
         System.out.print("Write a number for a sum: ");
         n1 = scn.nextInt();
-
         System.out.print("Write a new number: ");
         n2 = scn.nextInt();
         
