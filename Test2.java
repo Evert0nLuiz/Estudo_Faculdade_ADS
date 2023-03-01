708f2dcfafe6710e38b43f97d431c83c92e9d49c
Everton <76123738+Vzuviiu@users.noreply.github.com>
2023-02-28 21:29:14 -0300

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
-public class Test2
+public class Test2 
 {
-  public static void main(String[] args)
-  {
-    int[] maiorMenorArray;
-    maiorMenorArray = new int[5];
-    
-    int maior;
-    int menor;
-    
-    Scanner scn = new Scanner(System.in);
-    
-    for (int i = 0; i < maiorMenorArray.length; i++)
+    public static void main(String args[]) 
     {
-      System.out.println("Adicione um número pada fazer a comparação");
-      maiorMenorArray[i] = scn.nextInt();
+        Scanner scn = new Scanner(System.in);
+        int[] valores ;
+        valores = new int [5];
+        int valor;
+        int maior = 0;
+        int menor = 0;
+
+        for (int i = 0; i < valores.length; i++)
+        {
+            System.out.println("Me diga um number");
+            valores [i] = scn.nextInt();
+
+            for( int p = 0; p < valores.length; p++)
+            {
+                if (maior < valores[p])
+                    maior = valores[p] ;
+            }
+            for(int p = 0; p < valores.length; p++)
+            {
+                if (menor == 0)
+                    menor = valores [p];
+                    else if (menor > valores[p])
+                      menor = valores[p];
+            }
+        }          
+        System.out.println(maior);    
+        System.out.println(menor);  
+
     }
-    for (int i = 0; i < maiorMenorArray.length; i++)
-    {
-      System.out.println(maiorMenorArray[i]);
-    }
-  }
-}
+ 
